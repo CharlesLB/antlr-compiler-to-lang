cf21dba032c325c0c8525ac5df1e313a1dc46d47
GabriellaCarvalho0701 <gabriellacarvalho0701@gmail.com>
2024-09-01 14:01:51 -0300

    feat: Criação da inicialização de parâmetro conforme tipo

--- lang/ast/definitions/Param.java
+++ lang/ast/definitions/Param.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import lang.ast.Node;
 import lang.ast.expressions.ID;
 import lang.ast.statements.data.Decl;
+import lang.ast.types.Btype;
+import lang.ast.types.IDType;
+import lang.ast.types.MatrixType;
 import lang.symbols.DataTable;
-import lang.symbols.FunctionTable;
 
 public class Param extends Node {
 	private ID id;
@@ -58,22 +60,88 @@ public class Param extends Node {
 			return context.get(paramName);
 		}
 
-		System.out.println("Else Node Param - " + id.getName() + "  " + t);
+		// Verifica se o tipo é um Btype ou MatrixType de Btype
+		if (t instanceof Btype || (t instanceof MatrixType && ((MatrixType) t).getBaseType() instanceof Btype)) {
+			Object defaultValue = null;
+			String btypeName = t instanceof Btype ? t.toString() : ((MatrixType) t).getBaseType().toString();
 
-		Data data = DataTable.getInstance().getData(t.toString());
+			switch (btypeName) {
+				case "Int":
+					defaultValue = 0;
+					break;
+				case "Float":
+					defaultValue = 0.0f;
+					break;
+				case "Char":
+					defaultValue = '\0'; // Null character
+					break;
+				case "Bool":
+					defaultValue = false;
+					break;
+				default:
+					throw new RuntimeException("Tipo básico desconhecido: " + btypeName);
+			}
 
-		if (data == null) {
-			throw new RuntimeException("Estrutura de dados " + t.toString() + " não definida.");
+			context.put(paramName, defaultValue);
+			System.out.println("Parametro básico " + id.getName() + " registrado com tipo " + t);
+			return context.get(paramName);
 		}
+		// Verifica se o tipo é um MatrixType de Data
+		else if (t instanceof MatrixType && ((MatrixType) t).getBaseType() instanceof IDType) {
+			IDType baseType = (IDType) ((MatrixType) t).getBaseType();
+			Data data = DataTable.getInstance().getData(baseType.toString());
 
-		// Inicializa a estrutura Data
-		HashMap<String, Object> dataInstance = new HashMap<String, Object>();
-		for (Decl decl : data.getDeclarations()) {
-			dataInstance.put(decl.getID().getName(), decl.getType());
+			if (data == null) {
+				throw new RuntimeException("Estrutura de dados " + baseType.toString() + " não definida.");
+			}
+
+			// Inicializa uma matriz de HashMap para o Data
+			int dimensions = ((MatrixType) t).getDimensions();
+			Object[] dataMatrix = initializeDataMatrix(dimensions, data);
+			context.put(paramName, dataMatrix);
+			System.out.println("Estrutura de dados " + id.getName() + " inicializada como matriz.");
+
+			return context.get(paramName);
 		}
-		context.put(paramName, dataInstance);
-		System.out.println("Estrutura de dados " + id.getName() + " inicializada.");
+		// Verifica se o tipo é um Data comum
+		else {
+			System.out.println("Else Node Param - " + id.getName() + "  " + t);
 
-		return context.get(paramName);
+			Data data = DataTable.getInstance().getData(t.toString());
+
+			if (data == null) {
+				throw new RuntimeException("Estrutura de dados " + t.toString() + " não definida.");
+			}
+
+			// Inicializa a estrutura Data
+			HashMap<String, Object> dataInstance = new HashMap<String, Object>();
+			for (Decl decl : data.getDeclarations()) {
+				dataInstance.put(decl.getID().getName(), decl.getType());
+			}
+			context.put(paramName, dataInstance);
+			System.out.println("Estrutura de dados " + id.getName() + " inicializada.");
+
+			return context.get(paramName);
+		}
+	}
+
+	// Método auxiliar para inicializar uma matriz de Data
+	private Object[] initializeDataMatrix(int dimensions, Data data) {
+		if (dimensions == 1) {
+			HashMap<String, Object>[] dataArray = new HashMap[data.getDeclarations().size()];
+			for (int i = 0; i < dataArray.length; i++) {
+				dataArray[i] = new HashMap<>();
+				for (Decl decl : data.getDeclarations()) {
+					dataArray[i].put(decl.getID().getName(), decl.getType());
+				}
+			}
+			return dataArray;
+		} else {
+			Object[] outerArray = new Object[data.getDeclarations().size()];
+			for (int i = 0; i < outerArray.length; i++) {
+				outerArray[i] = initializeDataMatrix(dimensions - 1, data);
+			}
+			return outerArray;
+		}
 	}
 }
