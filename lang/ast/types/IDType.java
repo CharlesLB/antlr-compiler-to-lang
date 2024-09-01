00d4b93a9f9f9a9adbb8cd3bd9c79820416371c8
CharlesLB <charles.braga.jf.cb@gmail.com>
2024-09-01 14:05:08 -0300

    doc: Types

--- lang/ast/types/IDType.java
+++ lang/ast/types/IDType.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 
 import lang.ast.definitions.Type;
 
+/**
+ * Representa um tipo de dado.
+ * 
+ * @Parser type
+ * 
+ * @Example ID -> Vai contra a regra do Parser, mas funciona
+ */
 public class IDType extends Type {
 	private String id;
 
